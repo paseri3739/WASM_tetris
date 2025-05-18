c100c057ffc2561b3eeeeb816f75977f0a61ba2a
paseri3739 <tael43@icloud.com>
2025-05-19 00:23:50 +0900

    コードスタイルの改善: 不要な改行を削除し、インデントを統一

--- include/monad/IO.hpp
+++ include/monad/IO.hpp
@@ -1,27 +1,25 @@
 #ifndef B1245E63_B187_47E7_97D2_5299D4F3693A
 #define B1245E63_B187_47E7_97D2_5299D4F3693A
+
+#include <functional>
 template <typename T>
-class IO
-{
-public:
+class IO {
+   public:
     explicit IO(std::function<T()> action) : action_(std::move(action)) {}
 
     T run() const { return action_(); }
 
     template <typename F>
-    auto map(F f) const -> IO<decltype(f(std::declval<T>()))>
-    {
-        return IO<decltype(f(std::declval<T>()))>([=]
-                                                  { return f(action_()); });
+    auto map(F f) const -> IO<decltype(f(std::declval<T>()))> {
+        return IO<decltype(f(std::declval<T>()))>([=] { return f(action_()); });
     }
 
     template <typename F>
-    auto flatMap(F f) const -> decltype(f(std::declval<T>()))
-    {
+    auto flatMap(F f) const -> decltype(f(std::declval<T>())) {
         return f(action_());
     }
 
-private:
+   private:
     std::function<T()> action_;
 };
 
