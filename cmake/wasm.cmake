8d92261f114c009a5c7d1dd5bf4b1b0ed97c68e0
paseri3739 <tael43@icloud.com>
2025-05-18 14:52:47 +0900

    フラグを整理

--- cmake/wasm.cmake
+++ cmake/wasm.cmake
@@ -10,21 +10,15 @@ function(setup_wasm_target TARGET_NAME)
     # 出力を *.html (wasm + shell) にする
     set_target_properties(${TARGET_NAME} PROPERTIES SUFFIX ".html")
 
-    # SDL2 のヘッダーを sysroot から直接参照
-    target_include_directories(${TARGET_NAME} PRIVATE
-        ${CMAKE_SYSROOT}/include/SDL2
-    )
 
     # Emscripten 向けリンクオプション
     target_link_options(${TARGET_NAME} PRIVATE
         "-sUSE_SDL=2"
-        "-sUSE_SDL_IMAGE=0"
-        "-sUSE_SDL_TTF=0"
-        "-sUSE_SDL_MIXER=0"
+        "-sUSE_SDL_IMAGE=2"
+        "-sUSE_SDL_MIXER=2"
         "-sALLOW_MEMORY_GROWTH=1"
         "-sFULL_ES3=1"
         "-sASYNCIFY"
-        "-sNO_EXIT_RUNTIME=1"
         # 必要に応じて EXPORTED_FUNCTIONS を追加
         "-sEXPORTED_FUNCTIONS=['_main']"
     )
