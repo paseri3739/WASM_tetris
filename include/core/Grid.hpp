412a5447b13a7f420f559bd790754382e6cc2a90
paseri3739 <tael43@icloud.com>
2025-05-25 19:59:18 +0900

    TetrisGridFactoryクラスを追加し、TetrisGrid構造体に名前メンバを追加

--- include/core/Grid.hpp
+++ include/core/Grid.hpp
@@ -2,15 +2,34 @@
 #define B6A2B782_FE61_4667_84AD_802250E4C851
 
 #include <Cell.hpp>
+#include <GameConfig.hpp>
 #include <Position.hpp>
 
 struct TetrisGrid {
+    std::string name;
     Position position;
     double width;
     double height;
     int rows;
     int columns;
     std::vector<std::vector<Cell>> cells;
+};
+
+class TetrisGridFactory {
+   private:
+    CellFactory cell_factory;
+    GameConfig game_config;
+
+   public:
+    TetrisGridFactory(CellFactory cf, GameConfig gc)
+        : cell_factory(std::move(cf)), game_config(std::move(gc)) {}
+
+    TetrisGrid create_grid(const std::string& name, const Position& position, double width,
+                           double height, int rows, int columns) {
+        TetrisGrid grid{name, position, width, height, rows, columns, {}};
+        grid.cells.resize(rows, std::vector<Cell>(columns));
+        return grid;
+    }
 };
 
 #endif /* B6A2B782_FE61_4667_84AD_802250E4C851 */
