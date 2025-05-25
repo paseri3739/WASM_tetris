2af749490134f6716635fba758208f9b822db730
paseri3739 <tael43@icloud.com>
2025-05-25 19:37:11 +0900

    コメント

--- include/monad/IO.hpp
+++ include/monad/IO.hpp
@@ -5,11 +5,16 @@
 template <typename T>
 class IO {
    public:
+    // コンストラクタ 引数1つのコンストラクタにはexplicitをつける
     explicit IO(std::function<T()> action) : action_(std::move(action)) {}
 
+    // action_は () -> T の関数オブジェクトなので実行するとT型の値を返す
     T run() const { return action_(); }
 
     template <typename F>
+    // auto f() -> 型(後置) decltypeの中でdeclvalを使うことで、fの引数の型を取得(typeofのようなもの)
+    // declval<T>()は、T型の値を生成せずにT型の値を表現するためのもの
+    // (型がメソッドを持っているならこの後にdeclval<T>().メソッド名()と書くことでメソッドの戻り値の型を取得できる)
     auto map(F f) const -> IO<decltype(f(std::declval<T>()))> {
         return IO<decltype(f(std::declval<T>()))>([=] { return f(action_()); });
     }
@@ -20,7 +25,7 @@ class IO {
     }
 
    private:
-    std::function<T()> action_;
+    std::function<T()> action_;  // () -> T の関数オブジェクト
 };
 
 #endif /* B1245E63_B187_47E7_97D2_5299D4F3693A */
