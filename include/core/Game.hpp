088369b96e656fe4ee5eaea89f44e99ba2881899
paseri3739 <tael43@icloud.com>
2025-05-26 09:13:16 +0900

    Gameクラスを新規作成し、ゲーム設定の取得や初期化処理、更新処理を実装

---
+++ include/core/Game.hpp
@@ -0,0 +1,25 @@
+#ifndef CECD6737_285E_48BD_BE62_13103B0254DC
+#define CECD6737_285E_48BD_BE62_13103B0254DC
+
+#include <core/GameConfig.hpp>
+
+class Game {
+   public:
+    explicit Game(const GameConfig& config) : config_(config) {}
+
+    // ゲームの設定を取得
+    const GameConfig& getConfig() const { return config_; }
+
+    // ゲームの初期化処理
+    bool initialize();
+
+    // ゲームの更新処理
+    void update();
+
+    void run_loop();
+
+   private:
+    GameConfig config_;
+};
+
+#endif /* CECD6737_285E_48BD_BE62_13103B0254DC */
