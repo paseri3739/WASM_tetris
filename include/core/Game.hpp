34f4b2eb914dcd023d5d096e1e2ee0b8536825ab
paseri3739 <tael43@icloud.com>
2025-05-26 09:24:32 +0900

    APIを変更

--- include/core/Game.hpp
+++ include/core/Game.hpp
@@ -9,17 +9,15 @@ class Game {
 
     // ゲームの設定を取得
     const GameConfig& getConfig() const { return config_; }
-
     // ゲームの初期化処理
     bool initialize();
-
-    // ゲームの更新処理
-    void update();
-
     void runLoop();
 
    private:
     GameConfig config_;
+    // ゲームの更新処理
+    void update(double delta_time);
+    void processInput();
 };
 
 #endif /* CECD6737_285E_48BD_BE62_13103B0254DC */
