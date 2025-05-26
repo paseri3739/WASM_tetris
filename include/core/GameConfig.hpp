e74db4fbe410628eb3da71d4ffece77374b6598e
paseri3739 <tael43@icloud.com>
2025-05-26 11:08:03 +0900

    戻す

--- include/core/GameConfig.hpp
+++ include/core/GameConfig.hpp
@@ -1,8 +1,5 @@
 #ifndef A3705902_55D3_4E4F_B7C4_0899B5406266
 #define A3705902_55D3_4E4F_B7C4_0899B5406266
-
-#include <memory>
-
 struct GameConfig {
     struct {
         double width;
@@ -20,9 +17,6 @@ struct GameConfig {
 namespace game_config {
 // ゲームの設定を格納する構造体
 constexpr GameConfig defaultGameConfig = {.window = {300, 600}, .cell = {30}, .grid = {20, 10}};
-std::shared_ptr<GameConfig> createDefaultConfig() {
-    return std::make_shared<GameConfig>(defaultGameConfig);
-}
 }  // namespace game_config
 
 #endif /* A3705902_55D3_4E4F_B7C4_0899B5406266 */
