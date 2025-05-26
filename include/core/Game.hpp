f2947005ff19a7f820ee66218010744950e11173
paseri3739 <tael43@icloud.com>
2025-05-26 17:11:11 +0900

    GameクラスにSceneManagerを追加し、コンストラクタを修正。ゲームの更新処理を実装。

--- include/core/Game.hpp
+++ include/core/Game.hpp
@@ -2,10 +2,12 @@
 #define CECD6737_285E_48BD_BE62_13103B0254DC
 
 #include <core/GameConfig.hpp>
+#include <core/Scene.hpp>
 
 class Game {
    public:
-    explicit Game(const GameConfig& config) : config_(config) {}
+    explicit Game(const GameConfig& config, SceneManager& scene_manager)
+        : config_(config), scene_manager_(scene_manager) {}
 
     // ゲームの設定を取得
     const GameConfig& getConfig() const { return config_; }
@@ -15,6 +17,8 @@ class Game {
 
    private:
     GameConfig config_;
+    SceneManager& scene_manager_;
+    double last_update_time_ = 0.0;
     // ゲームの更新処理
     void update(double delta_time);
     void processInput();
--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -1,1 +1,20 @@
+#include <chrono>
 #include <core/Game.hpp>
+
+void Game::update(double delta_time) { this->scene_manager_.update(delta_time); }
+void Game::processInput() {
+    // TODO: 抽象入力を受け取り、現在のシーンに渡す
+}
+void Game::runLoop() {
+    while (true) {
+        auto now = std::chrono::system_clock::now();
+        auto delta_time =
+            std::chrono::duration<double>(now - std::chrono::system_clock::from_time_t(
+                                                    static_cast<time_t>(last_update_time_)))
+                .count();
+        last_update_time_ = std::chrono::system_clock::to_time_t(now);
+        this->processInput();
+        this->update(delta_time);
+        this->scene_manager_.render();
+    }
+}
