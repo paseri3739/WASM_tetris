7a30167d6d8ca4f6344f5c9695fa135e56511cbd
paseri3739 <tael43@icloud.com>
2025-05-27 19:55:28 +0900

    削除

--- include/IO/SDLInputPoller.hpp
+++ include/IO/SDLInputPoller.hpp
@@ -7,13 +7,6 @@
 class SDLInputPoller : public InputPoller {
    public:
     void poll(Input& input) override;
-
-   private:
-    // SDLのキーコードとInputKeyのマッピング。TODO: ここに必要なキーを追加していく。
-    std::unordered_map<SDL_Keycode, InputKey> key_map_ = {
-        {SDLK_LEFT, InputKey::LEFT}, {SDLK_RIGHT, InputKey::RIGHT}, {SDLK_UP, InputKey::UP},
-        {SDLK_DOWN, InputKey::DOWN}, {SDLK_ESCAPE, InputKey::QUIT},
-    };
 };
 
 #endif /* AAB054B7_A6D3_4E3E_A203_66DBAA015871 */
