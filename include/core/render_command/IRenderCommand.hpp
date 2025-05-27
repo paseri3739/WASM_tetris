15c4e685434a1537c654851e982262a940982265
paseri3739 <tael43@icloud.com>
2025-05-27 20:32:56 +0900

    修正

--- include/core/render_command/IRenderCommand.hpp
+++ include/core/render_command/IRenderCommand.hpp
@@ -1,7 +1,7 @@
 #ifndef B8EC3BF1_19F1_4FCC_94AA_4BA4F20AE598
 #define B8EC3BF1_19F1_4FCC_94AA_4BA4F20AE598
 
-#include <core/Renderer.hpp>
+#include <core/IRenderer.hpp>
 class IRenderCommand {
    public:
     virtual ~IRenderCommand() = default;
