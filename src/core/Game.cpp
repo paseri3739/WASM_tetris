6241c5de1be427d0efd1e3a9923f715e5c461b89
paseri3739 <tael43@icloud.com>
2025-05-28 09:38:34 +0900

    モックを作成

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -6,6 +6,21 @@
 void Game::update(double delta_time) { this->scene_manager_.update(delta_time); }
 void Game::processInput() {
     // TODO: 抽象入力を取得し、SceneManager経由で現在のシーンに渡す
+    Input input = {std::unordered_map<InputKey, InputState>{
+        // モック入力データ
+        // TODO: 実際はSDLInputPollerを利用して入力を取得する
+        {InputKey::UP, {false, false, false}},
+        {InputKey::DOWN, {false, false, false}},
+        {InputKey::LEFT, {false, false, false}},
+        {InputKey::RIGHT, {false, false, false}},
+        {InputKey::ROTATE_LEFT, {false, false, false}},
+        {InputKey::ROTATE_RIGHT, {false, false, false}},
+        {InputKey::DROP, {false, false, false}},
+        {InputKey::PAUSE, {false, false, false}},
+        {InputKey::QUIT, {false, false, false}},
+    }};
+    // SDLInputPollerを利用して入力を取得する処理を追加
+    this->scene_manager_.process_input(input);
 }
 void Game::runLoop() {
     using clock = std::chrono::steady_clock;
