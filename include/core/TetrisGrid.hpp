48f0547ca2aac9d7eacff2e13c17f965201b1a79
paseri3739 <tael43@icloud.com>
2025-05-29 16:56:13 +0900

    必要なメソッドを整理

--- include/core/TetrisGrid.hpp
+++ include/core/TetrisGrid.hpp
@@ -1,7 +1,5 @@
 #ifndef EAEC85BA_F694_47C7_AB0E_2FD093BD0A16
 #define EAEC85BA_F694_47C7_AB0E_2FD093BD0A16
-#ifndef F7B71047_1F3E_482C_9849_3CB6B22E22C3
-#define F7B71047_1F3E_482C_9849_3CB6B22E22C3
 
 #include <algorithm>
 #include <core/Cell.hpp>
@@ -38,7 +36,7 @@ class TetrisGrid {
     static tl::expected<TetrisGrid, std::string> create(const std::string& id, Position pos,
                                                         Size sz, int rows, int cols);
 
-    inline void TetrisGrid::render(IRenderer& renderer) {
+    inline void render(IRenderer& renderer) {
         // セルを描画する
         int columns = this->grid_size.column;
         int rows = this->grid_size.row;
@@ -64,7 +62,7 @@ class TetrisGrid {
     // 旧 tetris_grid::create() は削除するため宣言しない
 };
 
-namespace tetris_grid_manipulation {
+namespace tetris_grid {
 
 Position get_position_of_cell(const TetrisGrid& grid, const GridColumnRow& grid_position,
                               double cell_size);
@@ -72,37 +70,8 @@ Position get_position_of_cell(const TetrisGrid& grid, const GridColumnRow& grid_
 GridColumnRow get_grid_position_of_cell(const TetrisGrid& grid, const Position& cell_position,
                                         double cell_size);
 
-TetrisGrid map_tetrimino_to_grid(const TetrisGrid& grid, const Tetrimino& tetrimino,
-                                 CellFactory& cell_factory);
-
-TetrisGrid unmap_moving_cell(const TetrisGrid& grid, const CellFactory& cell_factory);
-
-TetrisGrid clear_filled_rows(const TetrisGrid& grid, CellFactory& cell_factory);
-
 bool is_within_bounds(const TetrisGrid& grid, int column, int row);
 
-bool is_within_bounds(const TetrisGrid& grid, const Tetrimino& tetrimino);
+}  // namespace tetris_grid
 
-typedef struct {
-    bool collided;
-    bool horizontal;
-} CollisionResult;
-
-CollisionResult check_collision(const TetrisGrid& grid, const Tetrimino& next,
-                                const Tetrimino& previous);
-
-bool is_collided_bottom(const TetrisGrid& grid, const Tetrimino& tetrimino);
-
-bool is_game_over(const TetrisGrid& grid, const Tetrimino& tetrimino);
-
-TetrisGrid fix_tetrimino(const TetrisGrid& grid, const Tetrimino& tetrimino);
-
-std::vector<GridColumnRow> get_tetrimino_map(const TetrisGrid& grid, const Tetrimino& tetrimino);
-
-std::vector<GridColumnRow> get_tetrimino_map_with_out_of_bounds(const TetrisGrid& grid,
-                                                                const Tetrimino& tetrimino);
-
-}  // namespace tetris_grid_manipulation
-#endif /* F7B71047_1F3E_482C_9849_3CB6B22E22C3 */
-
-#endif /* EAEC85BA_F694_47C7_AB0E_2FD093BD0A16 */
+#endif
--- src/core/TetrisGrid.cpp
+++ src/core/TetrisGrid.cpp
@@ -1,23 +1,22 @@
 #include <core/TetrisGrid.hpp>
 
-Position tetris_grid_manipulation::get_position_of_cell(const TetrisGrid& grid,
-                                                        const GridColumnRow& grid_position,
-                                                        double cell_size) {
+Position tetris_grid::get_position_of_cell(const TetrisGrid& grid,
+                                           const GridColumnRow& grid_position, double cell_size) {
     // グリッドの左上位置に、セルの位置を加算してセルの位置を計算
     return Position{grid.position.x + grid_position.column * cell_size,
                     grid.position.y + grid_position.row * cell_size};
 }
 
-GridColumnRow tetris_grid_manipulation::get_grid_position_of_cell(const TetrisGrid& grid,
-                                                                  const Position& cell_position,
-                                                                  double cell_size) {
+GridColumnRow tetris_grid::get_grid_position_of_cell(const TetrisGrid& grid,
+                                                     const Position& cell_position,
+                                                     double cell_size) {
     // グリッドの左上位置からセルの位置を引いて、セルの列と行を計算
     int column = static_cast<int>((cell_position.x - grid.position.x) / cell_size);
     int row = static_cast<int>((cell_position.y - grid.position.y) / cell_size);
     return GridColumnRow{column, row};
 }
 
-bool tetris_grid_manipulation::is_within_bounds(const TetrisGrid& grid, int column, int row) {
+bool tetris_grid::is_within_bounds(const TetrisGrid& grid, int column, int row) {
     bool isColumnValid = column >= 0 && column <= grid.grid_size.column;
     bool isRowValid = row >= 0 && row <= grid.grid_size.row;
     return isColumnValid && isRowValid;
