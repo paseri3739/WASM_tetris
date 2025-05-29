a6c1fc676096dabf3c02549d05061494d5c31710
paseri3739 <tael43@icloud.com>
2025-05-29 11:08:04 +0900

    TetrisRuleクラスとTetriminoTypeQueueクラスを追加し落下処理を実装

---
+++ include/core/TetrisRule.hpp
@@ -0,0 +1,25 @@
+#ifndef ADFD1949_02B3_4216_A6EB_C0B2714665E9
+#define ADFD1949_02B3_4216_A6EB_C0B2714665E9
+#include <core/Tetrimino.hpp>
+#include <vector>
+
+class TetriminoTypeQueue {
+   private:
+    std::vector<TetriminoType> queue_;
+    void initializeNextTetriminos(int count);
+    TetriminoType getRandomTetriminoType();
+
+   public:
+    TetriminoType getNext();
+};
+
+class TetrisRule {
+   private:
+    const int dropIntervalMillis = 1000;
+    double accumulatedDropTime = 0;
+
+   public:
+    Tetrimino drop_tetrimino(const Tetrimino& tetrimino, double delta_time) noexcept;
+};
+
+#endif /* ADFD1949_02B3_4216_A6EB_C0B2714665E9 */
---
+++ src/core/TetrisRule.cpp
@@ -0,0 +1,14 @@
+#include <core/TetrisRule.hpp>
+
+Tetrimino TetrisRule::drop_tetrimino(const Tetrimino& tetrimino, double delta_time) {
+    if (tetrimino.state == TetriminoStateType::PENDING) {
+        return tetrimino;
+    }
+
+    this->accumulatedDropTime += delta_time;
+    if (this->accumulatedDropTime >= this->dropIntervalMillis) {
+        this->accumulatedDropTime -= this->dropIntervalMillis;
+        return tetrimino::move(tetrimino, 0, 1);
+    }
+    return tetrimino;
+}
